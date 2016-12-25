88de8d97b3f4b0c7e48186eab9b257f0827e20f7
Tiberio <tiberiusferreira@gmail.com>
2016-12-25 21:52:35 +0100

    Added edit functionality

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -11,20 +11,33 @@ class ShopsController < ApplicationController
     params.require(:shop).permit(:chain, :name, :latitude, :longitude,
     :address, :city, :zip, :phone, :country_code)
   end
-
+  public
   def destroy
-    
+
       @shop = Shop.find(params[:id])
       @shop.destroy
       redirect_to '/shops', :notice => "The shop has been deleted"
   end
 
+  def update
+    @shop = Shop.find(params[:id])
+
+    if @shop.update(shop_params)
+      redirect_to '/shops'
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @shop = Shop.find(params[:id])
-end
+  end
 
+  def edit
+    @shop = Shop.find(params[:id])
+  end
 
-  public def create
+   def create
       @shop = Shop.new(shop_params)
       if @shop.save
         redirect_to '/shops'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # When getting /index from url call controller shops and it's method index
+  root 'shop#index'
   resources :shops
   # match '/shop/:id' => 'shop#show', via: [:get, :post]
 
-  root 'shop#index'
+
   #get '/list' => 'shops#list'
   #get '/shops/new' => 'shops#new'
   #post 'shops' => 'shops#create'
