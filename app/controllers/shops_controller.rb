ffef9cbb41f4ed277a5e0d65d239f65f53560efb
Tiberio <tiberiusferreira@gmail.com>
2016-12-25 20:37:28 +0100

    Switch to Ruby default routes for model. Added delete functionality

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,33 @@
 class ShopsController < ApplicationController
-  def list
+  def index
     @shops = Shop.all
     end
+  def new
+    @shop = Shop.new
+  end
 
+
+  private def shop_params
+    params.require(:shop).permit(:chain, :name, :latitude, :longitude,
+    :address, :city, :zip, :phone, :country_code)
+  end
+
+  def destroy
+    
+      @shop = Shop.find(params[:id])
+      @shop.destroy
+      redirect_to '/shops', :notice => "The shop has been deleted"
+  end
+
+  def show
+    @shop = Shop.find(params[:id])
 end
+
+
+  public def create
+      @shop = Shop.new(shop_params)
+      if @shop.save
+        redirect_to '/shops'
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # When getting /index from url call controller shops and it's method index
-  get '/index' => 'shops#list'
-  
+  resources :shops
+  # match '/shop/:id' => 'shop#show', via: [:get, :post]
+
+  root 'shop#index'
+  #get '/list' => 'shops#list'
+  #get '/shops/new' => 'shops#new'
+  #post 'shops' => 'shops#create'
 end
